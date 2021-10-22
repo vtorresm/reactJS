d403f775e1660eb33637c519a0bee283ef98c523
Victor Alberto Torres Mori <vtorremo@everis.com>
2021-10-22 16:24:01 -0500

    router-redux

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,50 +1,50 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import Layout from '../components/Layout';
-import Header from '../components/Header';
+import { connect } from 'react-redux';
+
 import Search from '../components/Search';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Categories from '../components/Categories';
-import Footer from '../components/Footer';
-import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
 
-const API = 'http://localhost:3000/initalState';
-
-const App = () => {
-  const initialState = useInitialState(API);
-  return initialState.length === 0 ? (
-    <h1>Loading...</h1>
-  ) : (
-    <Layout>
-      <Header />
+const Home = ({ myList, trends, originals }) => {
+  return (
+    <>
       <Search />
-      {initialState.mylist.length > 0 && (
+      {myList.length > 0 && (
         <Categories title='Mi lista'>
           <Carousel>
-            {initialState.mylist.map((item) => (
-              <CarouselItem key={item.id} {...item} />
+            {myList.map((item) => (
+              <CarouselItem key={item.id} {...item} isList />
             ))}
           </Carousel>
         </Categories>
       )}
       <Categories title='Tendencias'>
         <Carousel>
-          {initialState.trends.map((item) => (
+          {trends.map((item) => (
             <CarouselItem key={item.id} {...item} />
           ))}
         </Carousel>
       </Categories>
       <Categories title='Originales de Platfix'>
         <Carousel>
-          {initialState.originals.map((item) => (
+          {originals.map((item) => (
             <CarouselItem key={item.id} {...item} />
           ))}
         </Carousel>
       </Categories>
-      <Footer />
-    </Layout>
+    </>
   );
 };
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    myList: state.myList,
+    trends: state.trends,
+    originals: state.originals,
+  };
+};
+
+export default connect(mapStateToProps, null)(Home);
---
+++ src/reducers/index.js
@@ -0,0 +1,37 @@
+const reducer = (state, action) => {
+  switch (
+    action.type // Vamos a validar el type que habimaos mencionado que es como la descripción del action
+  ) {
+    case 'SET_FAVORITE':
+      return {
+        ...state, // Desestructuramos la variable state
+        myList: [...state.myList, action.payload], // Asignamos nuevos valores a mylist que fueron los que se trajeron del action. Si no desestructuramos la myList se sobrescribiran y no se añadiran nuevos favoritos
+      };
+    case 'DELETE_FAVORITE':
+      console.log('state.myList -> ', state.myList);
+      return {
+        ...state,
+        myList: state.myList.filter((item) => item.id !== action.payload),
+      };
+    case 'LOGIN_REQUEST':
+      return {
+        ...state,
+        user: action.payload,
+      };
+    case 'LOGOUT_REQUEST':
+      return {
+        ...state,
+        user: action.payload,
+      };
+    case 'REGISTER_REQUEST':
+      return {
+        ...state,
+        user: action.payload,
+      };
+    default:
+      // Caso por defecto si no cumple el action con ninguno
+      return state;
+  }
+};
+
+export default reducer;
